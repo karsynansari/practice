00deee0ff5616263b3a032d8c0091db8bf15af98
karsyn ansari <karsyn.ansari@gmail.com>
2022-07-05 10:00:50 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
+  const list = [
+    "Banana",
+    "Apple",
+    "Orange",
+    "Mango",
+    "Pineapple",
+    "Watermelon",
+  ]
 export default function App() {
+ const [filteredList, setFilteredList] = useState(list); 
+ function handleSearch(e) {
+    if (e.target.value === "") {
+      setFilteredList(list);
+    }
+//if there's a value in the input field, filter the original array and return an array of results that match that condition
+//set the filtered list state to the filtered list 
+const searchResults = list.filter(listItem => (listItem.toUpperCase().includes(e.target.value.toUpperCase())));
+setFilteredList(searchResults)
+
+ }
+ console.log(filteredList);
   return (
-    <div>App</div>
-  )
+    <>
+      <div id="box">
+        <div>Searching Stuff</div>
+        <input onChange={handleSearch} placeholder="type to filter" />
+      </div>
+     {filteredList.map((eachListItem) => (<ul>{eachListItem}</ul>))}
+     </>
+  );
 }
